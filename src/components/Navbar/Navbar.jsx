e630dc64cfa59d3babda314e4f691f74b537312d
MNoumanAli <mnoumanali001@gmail.com>
2024-10-06 13:41:07 +0500

    updated navbar structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 
 const Navbar = () => {
     return (
-        <nav className="pt-[15px] pb-[15px] pr-[23px] pl-[23px] border border-black border-solid rounded-full w-[75%] flex justify-between item-center">
-            <p className="text-[22px]">Hamza Bashir</p>
-            <span className="flex w-[30%] justify-between items-center">
+        <nav className="pt-[15px] pb-[15px] border border-b-black w-[100%] flex justify-between item-center pl-[15px] pr-[15px]">
+            <span className="flex w-[20%] justify-between items-center">
                 <p>Projects</p>
                 <p>Services</p>
                 <p>About</p>
+            </span>
+            <span className="w-[60%] flex justify-center items-center">
+            <   p className="text-[22px] text-bold">Hamza Bashir</p>
+            </span>
+            <span className="w-[20%] flex justify-end items-center">
+                <p className="w-[150px] bg-[black] rounded-full p-[10px] text-[white] text-center">Let's Connect</p>
             </span>
         </nav>
     )
